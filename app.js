7ddeeb59da0dc4edad1c2aff176ded5b46db3090
mthbarber <mthbarber@gmail.com>
2024-05-17 10:59:54 +0100

    Test manual pipeline approval

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-
-app.get('/', (req, res) => res.send('Hello Goose!!'));
+app.get('/', (req, res) => res.send('Hello Goose!!, We should have added manual approval now'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
